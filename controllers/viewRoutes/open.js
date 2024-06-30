e5ced73a1fbe1c67c7755a9043fd1411beadf166
Corey Yates <cqyates@gmail.com>
2024-06-30 16:51:05 -0400

    created empty view routes for rendering handlebars

--- controllers/viewRoutes/open.js
+++ controllers/viewRoutes/open.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.render("landing", {loggedIn: false})
+});
+router.get('/all', (req, res) => {
+  res.render("all-posts", {loggedIn: false})
+});
+router.get("/login", (req, res) => {
+  res.render("login", {loggedIn: false})
+});
+router.get("/signup", (req, res) => {
+  res.render("signup", {loggedIn: false})
+});
+
+router.get("/goodbye", (req, res) => {
+  res.render("goodbye")
+})
+
 module.exports = router;
