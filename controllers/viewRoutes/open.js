f491b368f106ca6df5a13bd0b38039e600a2c213
Corey Yates <cqyates@gmail.com>
2024-06-30 18:39:34 -0400

    created user accounts, signup, login, logout

--- controllers/viewRoutes/open.js
+++ controllers/viewRoutes/open.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("landing", {loggedIn: false, layout: "main"})
+  res.render("landing", {loggedIn: req.session.logged_in, layout: "main"})
 });
 router.get('/all', (req, res) => {
-  res.render("all-posts", {loggedIn: false})
+  res.render("all-posts", {loggedIn: req.session.logged_in, layout: "main"})
 });
 router.get("/login", (req, res) => {
-  res.render("login", {loggedIn: false})
+  res.render("login", {loggedIn: req.session.logged_in, layout: "main"})
 });
 router.get("/signup", (req, res) => {
-  res.render("signup", {loggedIn: false})
+  res.render("signup", {loggedIn: req.session.logged_in, layout: "main"})
 });
 
 router.get("/goodbye", (req, res) => {
